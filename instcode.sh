645e93258897c6baa216c75192c66b7841d581b8
ymmtmdk <ymmtmdk@gmail.com>
2014-09-19 20:53:56 +0900

    Using mv instead of tar.

--- instcode.sh
+++ instcode.sh
@@ -1,20 +1,23 @@
+mv sales_nologin ___sales_nologin___
+mv sales ___sales___
+mv fotoshare ___fotoshare___
 meteor create sales_nologin
 meteor create sales
 meteor create fotoshare
 cd sales_nologin
 rm *.html *.js *.css
-tar zxvf ../sales_nologin.tgz
+mv ../___sales_nologin___/* .
 cd ..
 cd sales
 meteor remove insecure autopublish
 meteor add accounts-password accounts-ui
 rm *.html *.js *.css
-tar zxvf ../sales.tgz
+mv ../___sales___/* .
 cd ..
 cd fotoshare
 meteor remove insecure autopublish
 meteor add accounts-password accounts-ui
 rm *.html *.js *.css
-tar zxvf ../fotoshare.tgz
+mv ../___fotoshare___/* .
 cd ..
-
+rmdir ___sales_nologin___ ___sales___ ___fotoshare___
